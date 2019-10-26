43372548cd2697c2e392981a4e9e543d669c52a4
Koji Shimba <me@shimba.co>
2019-10-27 03:54:02 +0900

    Setup frontend

---
+++ app/frontend/javascript/components/pages/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <transition name="app" mode="out-in">
+    <div v-if="state.work" key="content">
+    </div>
+    <div v-else key="loading">
+      <div class="d-flex justify-content-center align-items-center vh-100">
+        <div class="c-loading">
+          <div class="c-loading__core">
+            Loading...
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script lang="ts">
+  import $ from 'jquery'
+  import { createComponent, onMounted, reactive } from '@vue/composition-api'
+
+  export default createComponent({
+    setup(props, context) {
+      const state = reactive({})
+
+      return {
+        state,
+      }
+    }
+  })
+</script>
