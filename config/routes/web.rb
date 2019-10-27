8f51d3ebe460f91fbace811c27c2d3ceb268ba9f
Koji Shimba <me@shimba.co>
2019-10-28 03:13:46 +0900

    Add WelcomeController

--- config/routes/web.rb
+++ config/routes/web.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
-root "home#show"
+root "home#show", constraints: MemberConstraint.new
+root "welcome#show", constraints: GuestConstraint.new, as: nil # Set :as option to avoid two routes with the same name
