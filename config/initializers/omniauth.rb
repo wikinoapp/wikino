b3b2a30cbf86707e6e91beb40df9c83776ac8bb1
Koji Shimba <me@shimba.co>
2020-07-19 20:16:36 +0900

    Remove all files

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV.fetch("GOOGLE_CLIENT_ID"), ENV.fetch("GOOGLE_CLIENT_SECRET")
-end
-
-OmniAuth.config.full_host = ENV.fetch("NONOTO_URL")
