a29f728aaa5d92f950ed3f1427beb742c727dd0a
Koji Shimba <me@shimba.co>
2020-07-22 01:31:55 +0900

    Remove expose-loader

--- webpack.config.js
+++ webpack.config.js
@@ -41,19 +41,6 @@ module.exports = {
         ],
       },
       {
-        test: require.resolve('jquery'),
-        use: [
-          {
-            loader: 'expose-loader',
-            options: 'jQuery',
-          },
-          {
-            loader: 'expose-loader',
-            options: '$',
-          },
-        ],
-      },
-      {
         test: /\.scss$/,
         use: [
           {
