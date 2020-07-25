bd075030767e4ac505043c552c0c31f0340793ab
Koji Shimba <me@shimba.co>
2020-07-26 05:35:19 +0900

    Use CompositionEvent for Japanese IME

--- app/frontend/javascript/controllers/editor-controller.ts
+++ app/frontend/javascript/controllers/editor-controller.ts
@@ -12,13 +12,13 @@ export default class extends Controller {
   hintsTarget!: HTMLElement;
   textAreaTarget!: HTMLTextAreaElement;
   isHintsDisplayed!: boolean;
+  isComposing!: boolean;
+  cm!: Editor;
+  pos!: Position;
+  doc!: Doc;
 
   initialize() {
-    let cursor: Position;
-    let currentLine: string;
-    let doc: Doc;
-
-    const editor: EditorFromTextArea = CodeMirror.fromTextArea(this.textAreaTarget, {
+    this.cm = CodeMirror.fromTextArea(this.textAreaTarget, {
       mode: {
         name: 'gfm',
         highlightFormatting: true,
@@ -29,25 +29,26 @@ export default class extends Controller {
       indentWithTabs: false,
       tabSize: 2,
     });
+    this.doc = this.cm.getDoc();
+    this.pos = this.doc.getCursor();
 
-    editor.setOption('extraKeys', {
-      Tab: (cm: Editor) => {
-        if (cm.somethingSelected()) {
-          cm.indentSelection('add');
+    this.cm.setOption('extraKeys', {
+      Tab: () => {
+        if (this.cm.somethingSelected()) {
+          this.cm.indentSelection('add');
           return;
         }
 
-        cm.execCommand('insertSoftTab');
+        this.cm.execCommand('insertSoftTab');
       },
 
-      'Shift-Tab': (cm: Editor) => {
-        cm.indentSelection('subtract');
+      'Shift-Tab': () => {
+        this.cm.indentSelection('subtract');
       },
 
-      Enter: (cm: Editor) => {
-        doc = cm.getDoc();
-        const cursor = doc.getCursor();
-        const prevLine = doc.getLine(cursor.line);
+      Enter: () => {
+        console.log('enter!');
+        const prevLine = this.doc.getLine(this.pos.line);
 
         const trimmedPrevLine = prevLine.trim();
         const indentSpaceMatches = prevLine.match(/^ +/);
@@ -57,7 +58,7 @@ export default class extends Controller {
         let replacement = '';
         if (firstChar === '-' || firstChar === '*') {
           if (trimmedPrevLine === '-' || trimmedPrevLine === '*') {
-            doc.getLineHandle(cursor.line).text = '';
+            this.doc.getLineHandle(this.pos.line).text = '';
             replacement = `\n${indentSpace}`;
           } else {
             replacement = `\n${indentSpace}${firstChar} `;
@@ -66,29 +67,14 @@ export default class extends Controller {
           replacement = `\n${indentSpace}`;
         }
 
-        cm.replaceSelection(replacement);
+        this.cm.replaceSelection(replacement);
       },
     });
 
-    editor.on('inputRead', (cm: Editor, changeObj: any) => {
-      // console.log('event.key: ', event.key);
+    this.cm.on('inputRead', (_cm: Editor, changeObj: any) => {
       // console.log('changeObj: ', changeObj);
-      doc = cm.getDoc();
-      cursor = doc.getCursor();
-      // console.log('cursor: ', cursor);
       const inputChar = changeObj.text[0];
       // console.log('inputChar: ', inputChar);
-      currentLine = doc.getLine(cursor.line);
-      // console.log('currentLine: ', currentLine);
-      const prevChars = currentLine.slice(0, cursor.ch);
-      // console.log('prevChars: ', prevChars);
-      // console.log('changed! currentLine: ', currentLine);
-      // console.log('getSelection: ', doc.getSelection());
-
-      // let bracketStartPos: Position;
-      // if (inputChar === '[') {
-      //   bracketStartPos = changeObj.from;
-      // }
 
       [
         ['(', ')'],
@@ -96,46 +82,17 @@ export default class extends Controller {
         ['{', '}'],
       ].forEach((pair) => {
         if (inputChar === pair[0]) {
-          cm.replaceSelection(pair[1], 'start');
+          this.cm.replaceSelection(pair[1], 'start');
         }
       });
 
-      const linkTitle = this.symbolValue(prevChars, '[');
-      console.log('linkTitle: ', linkTitle);
-      if (linkTitle) {
-        // const cursorElm: HTMLElement | null = this.element.querySelector('.CodeMirror-cursor');
-        // console.log('cursorElm: ', cursorElm);
-
-        axios
-          .get('/api/internal/notes', {
-            params: {
-              q: linkTitle,
-            },
-          })
-          .then((res) => {
-            const hintsHtml = res.data ? res.data : null;
-            // console.log('hintsHtml: ', hintsHtml);
-            this.hintsTarget.innerHTML = hintsHtml;
-            this.isHintsDisplayed = true;
-            new EventDispatcher('editor-hints:show').dispatch();
-
-            const cursorCoords = cm.cursorCoords(false, 'window');
-            // console.log('cursorCoords: ', cursorCoords);
-            this.hintsTarget.style.left = `${cursorCoords.left - 10}px`;
-            this.hintsTarget.style.top = `${cursorCoords.top + 25}px`;
-            // const bracketStartCoords = cm.charCoords(bracketStartPos);
-            // console.log('bracketStartCoords: ', bracketStartCoords);
-          })
-          .catch((err) => {
-            console.log('err: ', err);
-          });
-      }
+      this.showHints(this.getPrevChars());
     });
 
-    editor.on('keydown', (cm: Editor, event: KeyboardEvent) => {
+    this.cm.on('keydown', (cm: Editor, event: KeyboardEvent) => {
       // console.log('keydown! event.code: ', event.code);
-      doc = cm.getDoc();
-      cursor = doc.getCursor();
+      this.doc = cm.getDoc();
+      this.pos = this.doc.getCursor();
 
       if (this.isHintsDisplayed && this.isKeyForHints(event.code)) {
         new EventDispatcher('editor-hints:keydown', { code: event.code }).dispatch();
@@ -143,36 +100,47 @@ export default class extends Controller {
       }
     });
 
+    this.element.addEventListener('compositionstart', () => {
+      // console.log('compositionstart!');
+      this.isComposing = true;
+    });
+
+    this.element.addEventListener('compositionend', () => {
+      // console.log('compositionend! this.pos: ', this.pos);
+      this.isComposing = false;
+      this.showHints(this.getPrevChars());
+    });
+
     document.addEventListener('editor:select-hint', (event: any) => {
       const { selectedNoteId, selectedNoteTitle } = event.detail;
       // console.log('selectedNoteId: ', selectedNoteId);
-      console.log('currentLine: ', currentLine);
-      console.log('cursor.ch: ', cursor.ch);
-      const prevChars = currentLine.slice(0, cursor.ch);
+      // console.log('currentLine: ', this.getCurrentLine());
+      // console.log('cursor.ch: ', this.pos.ch);
+      const prevChars = this.getCurrentLine().slice(0, this.pos.ch);
       // console.log('prevChars: ', prevChars);
       const startBracketMatch = this.reverseString(prevChars).match(/\[/);
-      console.log('startBracketMatch: ', startBracketMatch);
+      // console.log('startBracketMatch: ', startBracketMatch);
       let startBracketIndex: number = -1;
       if (typeof startBracketMatch?.index === 'number') {
-        startBracketIndex = cursor.ch - (startBracketMatch.index + 1);
-        console.log('startBracketIndex: ', startBracketIndex);
+        startBracketIndex = this.pos.ch - (startBracketMatch.index + 1);
+        // console.log('startBracketIndex: ', startBracketIndex);
       }
 
-      const afterChars = currentLine.slice(cursor.ch);
-      console.log('afterChars: ', afterChars);
+      const afterChars = this.getCurrentLine().slice(this.pos.ch);
+      // console.log('afterChars: ', afterChars);
       const endBracketMatch = afterChars.match(/]/);
-      console.log('endBracketMatch: ', endBracketMatch);
+      // console.log('endBracketMatch: ', endBracketMatch);
       let endBracketIndex: number = -1;
       if (typeof endBracketMatch?.index === 'number') {
-        endBracketIndex = cursor.ch + endBracketMatch.index;
-        console.log('endBracketIndex: ', endBracketIndex);
+        endBracketIndex = this.pos.ch + endBracketMatch.index;
+        // console.log('endBracketIndex: ', endBracketIndex);
       }
 
       if (startBracketIndex >= 0 && endBracketIndex >= 0) {
-        doc.replaceRange(
+        this.doc.replaceRange(
           `[${selectedNoteTitle}](/notes/${selectedNoteId})`,
-          { line: cursor.line, ch: startBracketIndex },
-          { line: cursor.line, ch: endBracketIndex + 1 },
+          { line: this.pos.line, ch: startBracketIndex },
+          { line: this.pos.line, ch: endBracketIndex + 1 },
         );
 
         this.isHintsDisplayed = false;
@@ -193,5 +161,44 @@ export default class extends Controller {
 
   reverseString(str: string) {
     return str.split('').reverse().join('');
+  }
+
+  getCurrentLine() {
+    return this.doc.getLine(this.pos.line);
+  }
+
+  getPrevChars() {
+    return this.getCurrentLine().slice(0, this.pos.ch);
+  }
+
+  showHints(prevChars: string) {
+    const linkTitle = this.symbolValue(prevChars, '[');
+    // console.log('linkTitle: ', linkTitle);
+
+    if (!this.isComposing && linkTitle) {
+      axios
+        .get('/api/internal/notes', {
+          params: {
+            q: linkTitle,
+          },
+        })
+        .then((res) => {
+          const hintsHtml = res.data ? res.data : null;
+          // console.log('hintsHtml: ', hintsHtml);
+          this.hintsTarget.innerHTML = hintsHtml;
+          this.isHintsDisplayed = true;
+          new EventDispatcher('editor-hints:show').dispatch();
+
+          const cursorCoords = this.cm.cursorCoords(false, 'window');
+          // console.log('cursorCoords: ', cursorCoords);
+          this.hintsTarget.style.left = `${cursorCoords.left - 10}px`;
+          this.hintsTarget.style.top = `${cursorCoords.top + 25}px`;
+          // const bracketStartCoords = cm.charCoords(bracketStartPos);
+          // console.log('bracketStartCoords: ', bracketStartCoords);
+        })
+        .catch((err) => {
+          console.log('err: ', err);
+        });
+    }
   }
 }
