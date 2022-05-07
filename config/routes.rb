b108d4311fa67fe8572e0407d1eb384c67c8c502
Koji Shimba <me@shimba.co>
2022-05-07 20:39:40 +0900

    Run `rails generate doorkeeper:install`

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  use_doorkeeper
   constraints(format: "json") do
     constraints(subdomain: "api") do
       match "/internal/graphql", via: :post, as: :internal_graphql, to: "internal/graphql#execute"
