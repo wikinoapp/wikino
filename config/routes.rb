f218f1bdbc2ab18f40478b18a851e737abd83518
Koji Shimba <me@shimba.co>
2022-05-08 05:01:56 +0900

    Remove local_graphql api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,8 @@
 
 Rails.application.routes.draw do
   use_doorkeeper
-  constraints(format: "json") do
-    constraints(subdomain: "api") do
-      match "/internal/graphql", via: :post, as: :internal_graphql, to: "internal/graphql#execute"
-    end
 
-    if Rails.env.development?
-      match "/local/graphql", via: :post, as: :local_graphql, to: "local/graphql#execute"
-    end
+  constraints(subdomain: "api") do
+    match "/internal/graphql", via: :post, as: :internal_graphql, to: "internal/graphql#execute"
   end
 end
