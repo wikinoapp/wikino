cefa4b31fc0b28bd0f0b5afddb007c6d07df7c72
Koji Shimba <me@shimba.co>
2022-05-24 04:47:07 +0900

    Run `bin/tapioca require`

--- sorbet/tapioca/require.rb
+++ sorbet/tapioca/require.rb
@@ -1,4 +1,15 @@
 # typed: true
 # frozen_string_literal: true
 
-# Add your extra requires here (`bin/tapioca require` can be used to boostrap this list)
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "active_job/railtie"
+require "active_model/railtie"
+require "active_record/railtie"
+require "active_support/core_ext/integer/time"
+require "bootsnap/setup"
+require "bundler/setup"
+require "github/markup"
+require "rails"
+require "rspec/rails"
