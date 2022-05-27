872804ff044105c9dc9a0d37897b6071829cc35a
Koji Shimba <me@shimba.co>
2022-05-28 00:15:27 +0900

    Change to nested class

--- app/controllers/concerns/internal/authenticatable.rb
+++ app/controllers/concerns/internal/authenticatable.rb
@@ -1,32 +1,34 @@
 # typed: strict
 # frozen_string_literal: true
 
-module Internal::Authenticatable
-  extend T::Sig
-  extend ActiveSupport::Concern
+module Internal
+  module Authenticatable
+    extend T::Sig
+    extend ActiveSupport::Concern
 
-  include ActionController::HttpAuthentication::Token::ControllerMethods
+    include ActionController::HttpAuthentication::Token::ControllerMethods
 
-  included do
-    before_action :authenticate_with_id_token
-  end
+    included do
+      before_action :authenticate_with_id_token
+    end
 
-  sig { returns(T.nilable(User)) }
-  attr_reader :current_user
+    sig { returns(T.nilable(User)) }
+    attr_reader :current_user
 
-  private
+    private
 
-  sig { returns(T.nilable(User)) }
-  def authenticate_with_id_token
-    @current_user = T.let(begin
-      authenticate_with_http_token do |id_token|
-        payload, _header = JsonWebToken.decode_id_token(id_token)
-        auth0_user_id = payload["sub"]
+    sig { returns(T.nilable(User)) }
+    def authenticate_with_id_token
+      @current_user = T.let(begin
+        authenticate_with_http_token do |id_token|
+          payload, _header = JsonWebToken.decode_id_token(id_token)
+          auth0_user_id = payload["sub"]
 
-        User.only_kept.where(auth0_user_id:).first_or_create!
-      end
-    rescue JWT::VerificationError, JWT::DecodeError
-      nil
-    end, T.nilable(User))
+          User.only_kept.where(auth0_user_id:).first_or_create!
+        end
+      rescue JWT::VerificationError, JWT::DecodeError
+        nil
+      end, T.nilable(User))
+    end
   end
 end
--- app/controllers/internal/graphql_controller.rb
+++ app/controllers/internal/graphql_controller.rb
@@ -1,44 +1,46 @@
 # typed: strict
 # frozen_string_literal: true
 
-class Internal::GraphqlController < ActionController::API
-  extend T::Sig
+module Internal
+  class GraphqlController < ActionController::API
+    extend T::Sig
 
-  include Internal::Authenticatable
+    include Internal::Authenticatable
 
-  sig { returns(T.untyped) }
-  def execute
-    variables = ensure_hash(params[:variables])
-    context = {
-      viewer: current_user
-    }
-    result = NonotoSchema.execute(params[:query].to_s, variables:, context:)
+    sig { returns(T.untyped) }
+    def execute
+      variables = ensure_hash(params[:variables])
+      context = {
+        viewer: current_user
+      }
+      result = NonotoSchema.execute(params[:query].to_s, variables:, context:)
 
-    render json: result
-  end
+      render json: result
+    end
 
-  private
+    private
 
-  sig do
-    params(
-      ambiguous_param: T.nilable(T.any(String, T::Hash[T.untyped, T.untyped], Numeric, ActionController::Parameters))
-    )
-      .returns(T.any(T::Hash[T.untyped, T.untyped], ActionController::Parameters))
-  end
-  def ensure_hash(ambiguous_param)
-    case ambiguous_param
-    when String
-      if ambiguous_param.present?
-        ensure_hash(JSON.parse(ambiguous_param))
-      else
+    sig do
+      params(
+        ambiguous_param: T.nilable(T.any(String, T::Hash[T.untyped, T.untyped], Numeric, ActionController::Parameters))
+      )
+        .returns(T.any(T::Hash[T.untyped, T.untyped], ActionController::Parameters))
+    end
+    def ensure_hash(ambiguous_param)
+      case ambiguous_param
+      when String
+        if ambiguous_param.present?
+          ensure_hash(JSON.parse(ambiguous_param))
+        else
+          {}
+        end
+      when Hash, ActionController::Parameters
+        ambiguous_param
+      when nil
         {}
+      else
+        raise(ArgumentError, "Unexpected parameter: #{ambiguous_param.inspect}")
       end
-    when Hash, ActionController::Parameters
-      ambiguous_param
-    when nil
-      {}
-    else
-      raise(ArgumentError, "Unexpected parameter: #{ambiguous_param.inspect}")
     end
   end
 end
