31ea8ededd43a17f11bdb665902dc41e5ae07847
Koji Shimba <me@shimba.co>
2022-05-28 23:16:21 +0900

    Fix NonotoSchema

--- app/graphql/nonoto_schema.rb
+++ app/graphql/nonoto_schema.rb
@@ -11,14 +11,13 @@ class NonotoSchema < GraphQL::Schema
 
   use GraphQL::Batch
 
-  sig { params(object: ApplicationRecord, _type: T.untyped, _ctx: T.untyped).returns(String) }
+  sig { params(object: ApplicationRecord, _type: T.untyped, _ctx: T.nilable(GraphQL::Query::Context)).returns(String) }
   def self.id_from_object(object, _type = nil, _ctx = nil)
     object.to_gid_param
   end
 
-  sig { params(node_id: String, ctx: GraphQL::Query::Context).returns(T.nilable(ApplicationRecord)) }
-  def self.object_from_id(node_id, ctx)
-    object = GlobalID.find(node_id)
-    Pundit.policy(ctx[:viewer], object)
+  sig { params(node_id: String, _ctx: T.nilable(GraphQL::Query::Context)).returns(T.nilable(ApplicationRecord)) }
+  def self.object_from_id(node_id, _ctx = nil)
+    GlobalID.find(node_id)
   end
 end
