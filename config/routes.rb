c446c9e1e499c02f30170ab1517dc20b0bcda8b3
Koji Shimba <me@shimba.co>
2022-10-31 04:02:13 +0900

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  constraints(subdomain: "api") do
-    match "/internal/graphql", via: :post, as: :internal_graphql, to: "internal/graphql#execute"
-  end
+  root "welcome#show"
 end
