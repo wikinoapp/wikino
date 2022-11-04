9b9859dddb157ec8038efd2536c5fca7ba461bfd
Koji Shimba <me@shimba.co>
2022-11-05 08:53:03 +0900

    Setup Auth0

---
+++ config/initializers/auth0.rb
@@ -0,0 +1,15 @@
+# typed: strict
+# frozen_string_literal: true
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider(
+    :auth0,
+    ENV.fetch("NONOTO_AUTH0_CLIENT_ID"),
+    ENV.fetch("NONOTO_AUTH0_CLIENT_SECRET"),
+    ENV.fetch("NONOTO_AUTH0_DOMAIN"),
+    callback_path: "/auth/callback",
+    authorize_params: {
+      scope: "openid profile"
+    }
+  )
+end
