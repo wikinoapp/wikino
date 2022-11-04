2c9f916c06b70883d7ae808e3150e9b69a3cf925
Koji Shimba <me@shimba.co>
2022-11-05 08:53:24 +0900

    Add columns to track sign in to users

---
+++ db/migrate/20221103142919_add_tracking_sign_in_fields_to_users.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddTrackingSignInFieldsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :sign_in_count, :integer, default: 0, null: false
+    add_column :users, :current_signed_in_at, :datetime
+    add_column :users, :last_signed_in_at, :datetime
+  end
+end
