cc7a8291dc51fd89118ed13a95831f11def63637
Koji Shimba <me@shimba.co>
2022-11-05 23:29:37 +0900

    Fix for types

--- app/controllers/concerns/authenticatable.rb
+++ app/controllers/concerns/authenticatable.rb
@@ -1,4 +1,4 @@
-# typed: false
+# typed: strict
 # frozen_string_literal: true
 
 module Authenticatable
@@ -9,16 +9,19 @@ module Authenticatable
     helper_method :sign_in_with_auth0_path, :sign_up_with_auth0_path
   end
 
+  sig { returns(String) }
   def sign_in_with_auth0_path
     "/auth/auth0"
   end
 
+  sig { returns(String) }
   def sign_up_with_auth0_path
     # ユーザ登録ページに直接遷移するために `screen_hint` パラメータを付与する
     # https://github.com/auth0/omniauth-auth0/pull/103
     "/auth/auth0?screen_hint=signup"
   end
 
+  sig { params(user: User).returns(String) }
   def sign_in(user)
     user.track_sign_in!
     session[:user_id] = user.id
---
+++ sorbet/manual/authenticatable.rbi
@@ -0,0 +1,7 @@
+# typed: strong
+# frozen_string_literal: true
+
+module Authenticatable
+  def self.helper_method(*methods); end
+  def session; end
+end
