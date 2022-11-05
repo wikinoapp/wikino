37fbb34cf0d86721b55c509132653268f36bb055
Koji Shimba <me@shimba.co>
2022-11-05 23:02:02 +0900

    Remove unnecessary self

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   # Deviseの #update_tracked_fields を参考にしている
   # https://github.com/heartcombo/devise/blob/451ff6d49c71e543962d2b29d77f2e744b2d47e1/lib/devise/models/trackable.rb#L20-L31
   def track_sign_in!(request)
-    old_current, new_current = self.current_signed_in_at, Time.now.utc
+    old_current, new_current = current_signed_in_at, Time.now.utc
     self.last_signed_in_at = old_current || new_current
     self.current_signed_in_at = new_current
 
