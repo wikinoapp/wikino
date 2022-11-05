2110a2447ced2b08cc8e637747298d004995a053
Koji Shimba <me@shimba.co>
2022-11-05 22:35:49 +0900

    Add `ApplicationController`

--- app/controllers/welcome/show_controller.rb
+++ app/controllers/welcome/show_controller.rb
@@ -1,7 +1,7 @@
 # typed: strict
 # frozen_string_literal: true
 
-class Welcome::ShowController < ActionController::Base
+class Welcome::ShowController < ApplicationController
   extend T::Sig
 
   sig { returns(T.untyped) }
