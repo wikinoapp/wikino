ba615247cb1cffcbc7752006d91813c1189a02af
Koji Shimba <me@shimba.co>
2022-11-12 05:36:52 +0900

    Start ujs

--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -1,5 +1,7 @@
 import 'bootstrap';
 import { Application } from '@hotwired/stimulus';
 import * as Turbo from '@hotwired/turbo';
+import ujs from '@rails/ujs';
 
+ujs.start();
 window.Stimulus = Application.start();
