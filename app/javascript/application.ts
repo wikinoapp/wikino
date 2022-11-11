0d788f83d078e4b122679165af0c9bbd7bb2301d
Koji Shimba <me@shimba.co>
2022-11-12 07:48:28 +0900

    Load Trix

--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -2,6 +2,7 @@ import { Application } from '@hotwired/stimulus';
 import * as Turbo from '@hotwired/turbo';
 import ujs from '@rails/ujs';
 import 'bootstrap';
+import 'trix';
 
 ujs.start();
 window.Stimulus = Application.start();
