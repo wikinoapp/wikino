332c65efb9059e3750056c9b65dd0799eddbe9e6
Koji Shimba <me@shimba.co>
2022-11-12 07:12:51 +0900

    Change to strict

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,4 +1,4 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
-UUID_FORMAT = /[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}/i.freeze
+UUID_FORMAT = T.let(/[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}/i.freeze, Regexp)
