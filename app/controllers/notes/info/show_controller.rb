68bcb8cd1f8e641217a83377b6f0cacf9fe70176
Koji Shimba <me@shimba.co>
2022-11-12 19:09:32 +0900

    Load note info

---
+++ app/controllers/notes/info/show_controller.rb
@@ -0,0 +1,13 @@
+# typed: true
+# frozen_string_literal: true
+
+class Notes::Info::ShowController < ApplicationController
+  include Authenticatable
+
+  before_action :authenticate_user
+
+  sig { returns(T.untyped) }
+  def call
+    @note = T.must(current_user).notes.find(params[:note_id])
+  end
+end
