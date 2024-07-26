1da4ac23d3e69ef5779d10ed29f8e61c5dcccefb
Koji Shimba <me@shimba.co>
2024-07-27 04:30:46 +0900

    WIP

--- db/migrate/20240000000002_initial_tables.rb
+++ db/migrate/20240000000002_initial_tables.rb
@@ -41,15 +41,6 @@ class InitialTables < ActiveRecord::Migration[7.1]
       t.timestamps
     end
 
-    create_table :actors, id: false do |t|
-      t.uuid :id, default: "generate_ulid()", null: false, primary_key: true
-      t.references :user, foreign_key: true, index: {unique: true}, null: false, type: :uuid
-      t.references :profile, foreign_key: true, index: {unique: true}, null: false, type: :uuid
-      t.timestamps
-
-      t.index %i[user_id profile_id], unique: true
-    end
-
     create_table :teams, id: false do |t|
       t.uuid :id, default: "generate_ulid()", null: false, primary_key: true
       t.references :profile, foreign_key: true, index: {unique: true}, null: false, type: :uuid
@@ -67,51 +58,60 @@ class InitialTables < ActiveRecord::Migration[7.1]
       t.uuid :id, default: "generate_ulid()", null: false, primary_key: true
       t.citext :identifier, index: {unique: true}, null: false
       t.string :name, default: "", null: false
+      t.string :visibility, null: false
     end
 
-    create_table :project_memberships, id: false do |t|
+    create_table :user_projects, id: false do |t|
+      t.uuid :id, default: "generate_ulid()", null: false, primary_key: true
+      t.references :user, foreign_key: true, null: false, type: :uuid
+      t.references :project, foreign_key: true, null: false, type: :uuid
+
+      t.index %i[user_id project_id], unique: true
+    end
+
+    create_table :team_projects, id: false do |t|
+      t.uuid :id, default: "generate_ulid()", null: false, primary_key: true
+      t.references :team, foreign_key: true, null: false, type: :uuid
+      t.references :project, foreign_key: true, null: false, type: :uuid
+
+      t.index %i[team_id project_id], unique: true
+    end
+
+    create_table :notes, id: false do |t|
       t.uuid :id, default: "generate_ulid()", null: false, primary_key: true
       t.references :project, foreign_key: true, null: false, type: :uuid
-      t.references :user, foreign_key: true, null: false, type: :uuid
-
-      t.index %i[project_id user_id], unique: true
-    end
-
-
-    create_table :notes, id: :uuid do |t|
-      t.references :user, null: false, foreign_key: true, type: :uuid
-      t.citext :title, null: false, default: ""
+      t.citext :title, default: "", null: false
       t.datetime :modified_at, null: false
       t.timestamps
-    end
-    add_index :notes, %i[user_id title], unique: true
-    add_index :notes, %i[user_id created_at]
-    add_index :notes, %i[user_id modified_at]
 
-    create_table :note_contents, id: :uuid do |t|
-      t.references :user, null: false, foreign_key: true, type: :uuid
-      t.references :note, null: false, foreign_key: true, type: :uuid
+      t.index %i[project_id title], unique: true
+      t.index %i[project_id created_at]
+      t.index %i[project_id modified_at]
+    end
+
+    create_table :note_contents, id: false do |t|
+      t.uuid :id, default: "generate_ulid()", null: false, primary_key: true
+      t.references :note, foreign_key: true, index: {unique: true}, null: false, type: :uuid
       t.citext :body, null: false, default: ""
       t.text :body_html, null: false, default: ""
       t.timestamps
     end
-    add_index :note_contents, %i[user_id note_id], unique: true
 
-    create_table :archived_note_contents, id: :uuid do |t|
-      t.references :user, null: false, foreign_key: true, type: :uuid
-      t.references :note, null: false, foreign_key: true, type: :uuid
+    create_table :past_note_contents, id: false do |t|
+      t.uuid :id, default: "generate_ulid()", null: false, primary_key: true
+      t.references :note, foreign_key: true, null: false, type: :uuid
       t.citext :body, null: false, default: ""
       t.text :body_html, null: false, default: ""
       t.timestamps
     end
-    add_index :archived_note_contents, %i[user_id note_id]
 
-    create_table :links, id: :uuid do |t|
-      t.references :note, null: false, foreign_key: true, type: :uuid
-      t.references :target_note, null: false, foreign_key: {to_table: :notes}, type: :uuid
+    create_table :note_links, id: false do |t|
+      t.uuid :id, default: "generate_ulid()", null: false, primary_key: true
+      t.references :source_note, foreign_key: {to_table: :notes}, null: false, type: :uuid
+      t.references :target_note, foreign_key: {to_table: :notes}, null: false, type: :uuid
       t.timestamps
+
+      t.index %i[source_note_id target_note_id], unique: true
     end
-    add_index :links, %i[note_id target_note_id], unique: true
-    add_index :links, :created_at
   end
 end
