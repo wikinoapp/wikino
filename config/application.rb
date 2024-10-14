1163a7373d83d31907fdafb88726f3d3e9d90bc6
Koji Shimba <me@shimba.co>
2024-10-14 17:54:09 +0900

    `active_storage/engine` を読み込む

--- config/application.rb
+++ config/application.rb
@@ -4,12 +4,14 @@
 require_relative "boot"
 
 require "rails"
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
+
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
+require "active_job/railtie"
+require "active_model/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
