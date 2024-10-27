9b9d3c57da0cf818f50daea14377434161743d3c
Koji Shimba <me@shimba.co>
2024-10-27 13:27:29 +0900

    WIP

--- app/models/page_location.rb
+++ app/models/page_location.rb
@@ -12,8 +12,8 @@ class PageLocation < T::Struct
 
   sig { params(current_space: Space, keys: T::Array[PageLocationKey]).returns(T::Array[PageLocation]) }
   def self.build_with_keys(current_space:, keys:)
-    topics = current_space.topics.where(name: keys.map(&:topic_name))
-    pages = current_space.pages.where(title: keys.map(&:page_title))
+    topics = current_space.topics.where(name: keys.map(&:topic_name).uniq)
+    pages = current_space.pages.where(title: keys.map(&:page_title).uniq)
 
     keys.each_with_object([]) do |key, ary|
       topic = topics.find { |topic| topic.name == key.topic_name }
