5564b19f55586ed65a6139ac432bd00a3730d0b3
Koji Shimba <me@shimba.co>
2024-11-08 03:06:27 +0900

    テストを追加

---
+++ spec/requests/email_confirmations/edit_spec.rb
@@ -0,0 +1,27 @@
+# typed: false
+# frozen_string_literal: true
+
+RSpec.describe "GET /email_confirmation/edit", type: :request do
+  it "EmailConfirmationのIDがセッションに格納されていないとき、トップページにリダイレクトすること" do
+    get("/email_confirmation/edit")
+
+    expect(response.status).to eq(302)
+    expect(response).to redirect_to("/")
+  end
+
+  it "EmailConfirmationのIDがセッションに格納されているとき、確認用コードの入力ページが表示されること" do
+    email_confirmation = create(:email_confirmation)
+
+    # EmailConfirmationのIDをセッションに格納する
+    post("/email_confirmation", params: {
+      new_email_confirmation_form: {
+        email: email_confirmation.email
+      }
+    })
+
+    get("/email_confirmation/edit")
+
+    expect(response.status).to eq(200)
+    expect(response.body).to include("確認用コード入力")
+  end
+end
