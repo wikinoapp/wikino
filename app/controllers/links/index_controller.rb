918456861f0df880b6a390125e391f753ef66b81
Koji Shimba <me@shimba.co>
2024-11-14 02:26:03 +0900

    リンクを返すエンドポイントを修正

--- app/controllers/links/index_controller.rb
+++ app/controllers/links/index_controller.rb
@@ -3,20 +3,26 @@
 
 module Links
   class IndexController < ApplicationController
+    include ControllerConcerns::SpaceSettable
     include ControllerConcerns::Authenticatable
     include ControllerConcerns::Authorizable
     include ControllerConcerns::Localizable
     include ControllerConcerns::PageSettable
 
+    layout false
+
     around_action :set_locale
-    before_action :require_authentication
+    before_action :set_current_space
+    before_action :restore_session
     before_action :set_page
+
+    rescue_from Pundit::NotAuthorizedError, with: :render_404
 
     sig { returns(T.untyped) }
     def call
       authorize(@page, :show?)
 
-      draft_page = Current.user!.draft_pages.find_by(page: @page)
+      draft_page = Current.user&.draft_pages&.find_by(page: @page)
       pageable = draft_page.presence || @page
 
       @link_collection = pageable.fetch_link_collection(after: params[:after])
