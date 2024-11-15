4d80d1e971f3e18b15adabf2cf5a871ad08a5794
Koji Shimba <me@shimba.co>
2024-11-15 19:27:34 +0900

    灰色をSlateで統一する

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,32 @@ module.exports = {
     './public/*.html',
   ],
   theme: {
-    extend: {},
+    // https://tailwindcss.com/docs/customizing-colors
+    colors: {
+      inherit: colors.inherit,
+      current: colors.current,
+      transparent: colors.transparent,
+      black: colors.black,
+      white: colors.white,
+      gray: colors.slate, // 統一感を出すため、灰色はSlateだけを使う
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose,
+    },
   },
   plugins: [
     require('daisyui'),
