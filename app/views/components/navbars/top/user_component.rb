f7ab1403bcf59f84bf0e91530cda3d9c3e6dc7a4
Koji Shimba <me@shimba.co>
2025-01-19 19:11:54 +0900

    WIP

--- app/views/components/navbars/top/user_component.rb
+++ app/views/components/navbars/top/user_component.rb
@@ -4,15 +4,10 @@
 module Navbars
   module Top
     class UserComponent < ApplicationComponent
-      sig { params(current_user: User, class_name: String).void }
-      def initialize(current_user:, class_name: "")
-        @current_user = current_user
+      sig { params(class_name: String).void }
+      def initialize(class_name: "")
         @class_name = class_name
       end
-
-      sig { returns(User) }
-      attr_reader :current_user
-      private :current_user
 
       sig { returns(String) }
       attr_reader :class_name
