6cdae4bbe692141261a85aae6d308e5fa5b34638
Koji Shimba <me@shimba.co>
2025-02-06 19:18:59 +0900

    テストがコケたとき差分がわかりやすいようにする

--- spec/models/markup_spec.rb
+++ spec/models/markup_spec.rb
@@ -38,39 +38,113 @@ RSpec.describe Markup, type: :model do
     page_1 = create(:page, space:, topic: topic_1, title: "Page 1")
     page_2 = create(:page, space:, topic: topic_2, title: "Page 2")
     page_3 = create(:page, space:, topic: topic_1, title: "Notebook -> List")
-    actual = Markup.new(current_topic: topic_1).render_html(
-      text: [
-        "- Test",
-        "- [[Page 1]]",
-        "- [[トピック1/Page 1]]",
-        # Page 2はトピック2に属しているのでリンクにならないはず
-        "- [[トピック1/Page 2]]",
-        "- [[Page 2]]",
-        "- [[トピック2/Page 2]]",
-        "- [[存在しないページ]]",
-        # `->` が含まれているとリンクにならないことがあったので追加 (リンクになるべき)
-        "- [[Notebook -> List]]",
-        "\n",
-        "文中にページリンクがある場合[[Page 1]]のテスト",
-        "\n",
-        "文中にトピック付きのページリンクがある場合[[トピック1/Page 1]]のテスト"
-      ].join("\n")
-    )
+
+    text = <<~HTML
+      - Test
+    HTML
+    actual = Markup.new(current_topic: topic_1).render_html(text:)
     expected = <<~HTML
       <ul>
         <li>Test</li>
+      </ul>
+    HTML
+    expect(normalize_html(actual)).to eq(normalize_html(expected))
+
+    text = <<~HTML
+      - [[Page 1]]
+    HTML
+    actual = Markup.new(current_topic: topic_1).render_html(text:)
+    expected = <<~HTML
+      <ul>
         <li><a class="link link-primary" href="/s/#{space.identifier}/pages/#{page_1.number}">Page 1</a></li>
+      </ul>
+    HTML
+    expect(normalize_html(actual)).to eq(normalize_html(expected))
+
+    text = <<~HTML
+      - [[トピック1/Page 1]]
+    HTML
+    actual = Markup.new(current_topic: topic_1).render_html(text:)
+    expected = <<~HTML
+      <ul>
         <li><a class="link link-primary" href="/s/#{space.identifier}/pages/#{page_1.number}">Page 1</a></li>
+      </ul>
+    HTML
+    expect(normalize_html(actual)).to eq(normalize_html(expected))
+
+    text = <<~HTML
+      - [[トピック1/Page 2]]
+    HTML
+    actual = Markup.new(current_topic: topic_1).render_html(text:)
+    # Page 2はトピック2に属しているのでリンクにならないはず
+    expected = <<~HTML
+      <ul>
         <li>[[トピック1/Page 2]]</li>
+      </ul>
+    HTML
+    expect(normalize_html(actual)).to eq(normalize_html(expected))
+
+    text = <<~HTML
+      - [[Page 2]]
+    HTML
+    actual = Markup.new(current_topic: topic_1).render_html(text:)
+    expected = <<~HTML
+      <ul>
         <li>[[Page 2]]</li>
+      </ul>
+    HTML
+    expect(normalize_html(actual)).to eq(normalize_html(expected))
+
+    text = <<~HTML
+      - [[トピック2/Page 2]]
+    HTML
+    actual = Markup.new(current_topic: topic_1).render_html(text:)
+    expected = <<~HTML
+      <ul>
         <li><a class="link link-primary" href="/s/#{space.identifier}/pages/#{page_2.number}">Page 2</a></li>
+      </ul>
+    HTML
+    expect(normalize_html(actual)).to eq(normalize_html(expected))
+
+    text = <<~HTML
+      - [[存在しないページ]]
+    HTML
+    actual = Markup.new(current_topic: topic_1).render_html(text:)
+    expected = <<~HTML
+      <ul>
         <li>[[存在しないページ]]</li>
+      </ul>
+    HTML
+    expect(normalize_html(actual)).to eq(normalize_html(expected))
+
+    # `->` が含まれているとリンクにならないことがあったので追加 (リンクになるべき)
+    text = <<~HTML
+      - [[Notebook -> List]]
+    HTML
+    actual = Markup.new(current_topic: topic_1).render_html(text:)
+    expected = <<~HTML
+      <ul>
         <li><a class="link link-primary" href="/s/#{space.identifier}/pages/#{page_3.number}">Notebook -&gt; List</a></li>
       </ul>
+    HTML
+    expect(normalize_html(actual)).to eq(normalize_html(expected))
+
+    text = <<~HTML
+      文中にページリンクがある場合[[Page 1]]のテスト
+    HTML
+    actual = Markup.new(current_topic: topic_1).render_html(text:)
+    expected = <<~HTML
       <p>文中にページリンクがある場合<a class="link link-primary" href="/s/#{space.identifier}/pages/#{page_1.number}">Page 1</a>のテスト</p>
+    HTML
+    expect(normalize_html(actual)).to eq(normalize_html(expected))
+
+    text = <<~HTML
+      文中にトピック付きのページリンクがある場合[[トピック1/Page 1]]のテスト
+    HTML
+    actual = Markup.new(current_topic: topic_1).render_html(text:)
+    expected = <<~HTML
       <p>文中にトピック付きのページリンクがある場合<a class="link link-primary" href="/s/#{space.identifier}/pages/#{page_1.number}">Page 1</a>のテスト</p>
     HTML
-
     expect(normalize_html(actual)).to eq(normalize_html(expected))
   end
 end
