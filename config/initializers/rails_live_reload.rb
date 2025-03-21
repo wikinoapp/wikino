f5072e68e9f85c4b5a6efdb95cbeda622bbf06aa
Koji Shimba <me@shimba.co>
2025-03-22 03:46:12 +0900

    Rails Live Reloadの設定を調整

--- config/initializers/rails_live_reload.rb
+++ config/initializers/rails_live_reload.rb
@@ -5,14 +5,13 @@ if defined?(RailsLiveReload)
   RailsLiveReload.configure do |config|
     # config.url = "/rails/live/reload"
 
-    # Default watched folders & files
-    config.watch %r{app/views/.+\.erb$}
-    config.watch %r{app/components/.+\.erb$}, reload: :always
-    config.watch %r{(app|vendor)/(assets|javascript)/\w+/(.+\.(css|js|html|png|jpg|ts|jsx)).*}, reload: :always
+    config.watch %r{app/views/.+\.(erb|rb)$}, reload: :always
+    config.watch %r{app/components/.+\.(erb|rb)$}, reload: :always
+    config.watch %r{(app|vendor)/(assets|javascript)/\w+/(.+\.(css|js|html|png|jpg|ts)).*}, reload: :always
+    config.watch %r{config/locales/.+\.yml}, reload: :always
 
     # More examples:
     # config.watch %r{app/helpers/.+\.rb}, reload: :always
-    # config.watch %r{config/locales/.+\.yml}, reload: :always
 
     # config.enabled = Rails.env.development?
   end
