3a9a4d2c9e7fa451159b0cb07ef4a8961e3944cd
Koji Shimba <me@shimba.co>
2025-04-10 18:37:51 +0900

    WIP

--- app/validators/filename_safe_validator.rb
+++ app/validators/filename_safe_validator.rb
@@ -6,6 +6,10 @@ class FilenameSafeValidator < ActiveModel::EachValidator
 
   # ファイル名として使用できない文字を含むかどうかをチェックする正規表現
   INVALID_CHARS_PATTERN = %r{[/\\:*?"<>|]}
+  WINDOWS_RESERVED_NAMES = %w[
+    CON PRN AUX NUL COM1 COM2 COM3 COM4 COM5 COM6 COM7 COM8 COM9
+    LPT1 LPT2 LPT3 LPT4 LPT5 LPT6 LPT7 LPT8 LPT9
+  ]
 
   sig { params(record: T.untyped, attribute: Symbol, value: T.nilable(String)).void }
   def validate_each(record, attribute, value)
@@ -18,6 +22,11 @@ class FilenameSafeValidator < ActiveModel::EachValidator
     # 先頭・末尾のスペースとピリオドのチェック
     if value.start_with?(" ", ".") || value.end_with?(" ", ".")
       record.errors.add(attribute, :cannot_start_or_end_with_space_or_dot)
+    end
+
+    # Windowsの予約語チェック
+    if WINDOWS_RESERVED_NAMES.include?(value.upcase)
+      record.errors.add(attribute, :reserved)
     end
   end
 end
