90e30247f0f66d484ce786d066f4788de3c8bd15
Koji Shimba <me@shimba.co>
2025-04-26 15:40:53 +0900

    Fix

--- app/views/pages/show_view.rb
+++ app/views/pages/show_view.rb
@@ -5,46 +5,46 @@ module Pages
   class ShowView < ApplicationView
     sig do
       params(
-        current_user_entity: T.nilable(UserEntity),
-        page_entity: PageEntity,
-        link_list_entity: LinkListEntity,
-        backlink_list_entity: BacklinkListEntity
+        current_user: T.nilable(User),
+        page: Page,
+        link_list: LinkList,
+        backlink_list: BacklinkList
       ).void
     end
-    def initialize(current_user_entity:, page_entity:, link_list_entity:, backlink_list_entity:)
-      @current_user_entity = current_user_entity
-      @page_entity = page_entity
-      @link_list_entity = link_list_entity
-      @backlink_list_entity = backlink_list_entity
+    def initialize(current_user:, page:, link_list:, backlink_list:)
+      @current_user = current_user
+      @page = page
+      @link_list = link_list
+      @backlink_list = backlink_list
     end
 
     sig { override.void }
     def before_render
-      title = I18n.t("meta.title.pages.show", space_name: space_entity.name, page_title: page_entity.title)
+      title = I18n.t("meta.title.pages.show", space_name: space.name, page_title: page.title)
       helpers.set_meta_tags(title:, **default_meta_tags(site: false))
     end
 
-    sig { returns(T.nilable(UserEntity)) }
-    attr_reader :current_user_entity
-    private :current_user_entity
+    sig { returns(T.nilable(User)) }
+    attr_reader :current_user
+    private :current_user
 
-    sig { returns(PageEntity) }
-    attr_reader :page_entity
-    private :page_entity
+    sig { returns(Page) }
+    attr_reader :page
+    private :page
 
-    sig { returns(LinkListEntity) }
-    attr_reader :link_list_entity
-    private :link_list_entity
+    sig { returns(LinkList) }
+    attr_reader :link_list
+    private :link_list
 
-    sig { returns(BacklinkListEntity) }
-    attr_reader :backlink_list_entity
-    private :backlink_list_entity
+    sig { returns(BacklinkList) }
+    attr_reader :backlink_list
+    private :backlink_list
 
-    delegate :space_entity, :topic_entity, to: :page_entity
+    delegate :space, :topic, to: :page
 
     sig { returns(T::Boolean) }
     private def signed_in?
-      !current_user_entity.nil?
+      !current_user.nil?
     end
 
     sig { returns(PageName) }
