25f01a5778a591141b1e87a74fee8fd3b18e3cf0
Koji Shimba <me@shimba.co>
2025-04-27 01:46:04 +0900

    Fix

--- app/views/spaces/settings/show_view.rb
+++ app/views/spaces/settings/show_view.rb
@@ -4,20 +4,15 @@
 module Spaces
   module Settings
     class ShowView < ApplicationView
-      sig do
-        params(
-          current_user: User,
-          space_entity: SpaceEntity
-        ).void
-      end
-      def initialize(current_user:, space_entity:)
+      sig { params(current_user: User, space: Space).void }
+      def initialize(current_user:, space:)
         @current_user = current_user
-        @space_entity = space_entity
+        @space = space
       end
 
       sig { override.void }
       def before_render
-        title = I18n.t("meta.title.spaces.settings.show", space_name: space_entity.name)
+        title = I18n.t("meta.title.spaces.settings.show", space_name: space.name)
         helpers.set_meta_tags(title:, **default_meta_tags)
       end
 
@@ -25,9 +20,9 @@ module Spaces
       attr_reader :current_user
       private :current_user
 
-      sig { returns(SpaceEntity) }
-      attr_reader :space_entity
-      private :space_entity
+      sig { returns(Space) }
+      attr_reader :space
+      private :space
 
       sig { returns(PageName) }
       private def current_page_name
