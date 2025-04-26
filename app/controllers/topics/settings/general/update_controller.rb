e0e3ed6da5a4196fcc9ca263c4f5dbc69de56abe
Koji Shimba <me@shimba.co>
2025-04-27 02:44:43 +0900

    Fix

--- app/controllers/topics/settings/general/update_controller.rb
+++ app/controllers/topics/settings/general/update_controller.rb
@@ -14,31 +14,38 @@ module Topics
         sig { returns(T.untyped) }
         def call
           space_record = SpaceRecord.find_by_identifier!(params[:space_identifier])
-          space_member_record = current_user!.space_member_record(space_record:)
-          topic = space_viewer.showable_topics.find_by!(number: params[:topic_number])
-          topic_entity = topic.to_entity(space_viewer:)
+          space_member_record = current_user_record!.space_member_record(space_record:)
+          space_member_policy = SpaceMemberPolicy.new(
+            user_record: current_user_record!,
+            space_member_record:
+          )
+          topic_record = space_member_policy.showable_topics(space_record:).find_by!(
+            number: params[:topic_number]
+          )
 
-          unless topic_entity.viewer_can_update?
+          unless space_member_policy.can_update_topic?(topic_record:)
             return render_404
           end
 
-          form = EditTopicForm.new(form_params.merge(topic_record: topic))
+          form = EditTopicForm.new(form_params.merge(topic_record:))
 
           if form.invalid?
-            return render(
-              Topics::Settings::General::ShowView.new(
-                current_user: current_user!,
-                topic_entity:,
-                form:
-              ),
+            topic = TopicRepository.new.to_model(topic_record:)
+
+            return render(Topics::Settings::General::ShowView.new(current_user:, topic:, form:), {
               status: :unprocessable_entity
-            )
+            })
           end
 
-          UpdateTopicService.new.call(form:)
+          UpdateTopicService.new.call(
+            topic_record:,
+            name: form.name.not_nil!,
+            description: form.description.not_nil!,
+            visibility: form.visibility.not_nil!
+          )
 
           flash[:notice] = t("messages.topics.updated")
-          redirect_to topic_settings_general_path(space.identifier, topic.number)
+          redirect_to topic_settings_general_path(space_record.identifier, topic_record.number)
         end
 
         sig { returns(ActionController::Parameters) }
