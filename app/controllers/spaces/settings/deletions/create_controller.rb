11582618ef0b4ff523b00cf269e0ce57959e59b4
Koji Shimba <me@shimba.co>
2025-05-02 18:36:12 +0900

    Fix

--- app/controllers/spaces/settings/deletions/create_controller.rb
+++ app/controllers/spaces/settings/deletions/create_controller.rb
@@ -5,18 +5,51 @@ module Spaces
   module Settings
     module Deletions
       class CreateController < ApplicationController
-        extend T::Sig
+        include ControllerConcerns::Authenticatable
+        include ControllerConcerns::Localizable
 
-        sig { void }
+        around_action :set_locale
+        before_action :require_authentication
+
+        sig { returns(T.untyped) }
         def call
-          @space = Space.find_by!(identifier: params[:space_identifier])
+          space_record = SpaceRecord.find_by_identifier!(params[:space_identifier])
+          space_member_record = current_user_record!.space_member_record(space_record:)
+          space_member_policy = SpaceMemberPolicy.new(
+            user_record: current_user_record!,
+            space_member_record:
+          )
 
-          if @space.name == params[:space_name]
-            @space.destroy!
-            redirect_to root_path, notice: "スペースを削除しました"
-          else
-            redirect_to space_settings_new_deletion_path(@space), alert: "スペース名が正しくありません"
+          unless space_member_policy.can_update_space?(space_record:)
+            return render_404
           end
+
+          form = SpaceDestroyConfirmationForm.new(form_params.merge(space_record:))
+
+          if form.invalid?
+            space = SpaceRepository.new.to_model(space_record:)
+
+            return render(
+              Spaces::Settings::Deletions::NewView.new(
+                current_user: current_user!,
+                space:,
+                form:
+              ),
+              status: :unprocessable_entity
+            )
+          end
+
+          SoftDestroySpaceService.new.call(space_record:)
+
+          flash[:notice] = t("messages.spaces.deleted")
+          redirect_to root_path
+        end
+
+        sig { returns(ActionController::Parameters) }
+        private def form_params
+          T.cast(params.require(:space_destroy_confirmation_form), ActionController::Parameters).permit(
+            :space_name
+          )
         end
       end
     end
