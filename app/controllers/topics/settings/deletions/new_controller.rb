c4ccd1192f391efe7d03ec57c308fbf4e50fb4f8
Koji Shimba <me@shimba.co>
2025-05-02 15:36:10 +0900

    Fix

---
+++ app/controllers/topics/settings/deletions/new_controller.rb
@@ -0,0 +1,42 @@
+# typed: true
+# frozen_string_literal: true
+
+module Topics
+  module Settings
+    module Deletions
+      class NewController < ApplicationController
+        include ControllerConcerns::Authenticatable
+        include ControllerConcerns::Localizable
+
+        around_action :set_locale
+        before_action :require_authentication
+
+        sig { returns(T.untyped) }
+        def call
+          space_record = SpaceRecord.find_by_identifier!(params[:space_identifier])
+          space_member_record = current_user_record!.space_member_record(space_record:)
+          space_member_policy = SpaceMemberPolicy.new(
+            user_record: current_user_record!,
+            space_member_record:
+          )
+          topic_record = space_member_policy.showable_topics(space_record:).find_by!(
+            number: params[:topic_number]
+          )
+
+          unless space_member_policy.can_update_topic?(topic_record:)
+            return render_404
+          end
+
+          topic = TopicRepository.new.to_model(topic_record:)
+          form = TopicDestroyConfirmationForm.new(topic_record:)
+
+          render Topics::Settings::Deletions::NewView.new(
+            current_user: current_user!,
+            topic:,
+            form:
+          )
+        end
+      end
+    end
+  end
+end
