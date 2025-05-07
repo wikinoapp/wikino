8f188a74f52963c4b11a35e90db0717ac679def1
Koji Shimba <me@shimba.co>
2025-05-08 01:42:55 +0900

    Fix

--- app/forms/account_form/destroy_confirmation.rb
+++ app/forms/account_form/destroy_confirmation.rb
@@ -11,6 +11,7 @@ module AccountForm
     validates :user_record, presence: true
     validates :user_atname, presence: true
     validate :user_atname_correct
+    validate :user_has_no_active_spaces
 
     sig { void }
     private def user_atname_correct
@@ -18,6 +19,14 @@ module AccountForm
       return if user_record.not_nil!.atname == user_atname
 
       errors.add(:user_atname, :incorrect)
+    end
+
+    sig { void }
+    private def user_has_no_active_spaces
+      return if user_record.nil?
+      return unless user_record.not_nil!.active_space_records.exists?
+
+      errors.add(:user_record, :has_active_spaces)
     end
   end
 end
