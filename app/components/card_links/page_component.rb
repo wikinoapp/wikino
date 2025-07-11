e038ba967c17349ad41cfa90711cf1360da6940b
Koji Shimba <me@shimba.co>
2025-07-12 01:00:20 +0900

    Fix

--- app/components/card_links/page_component.rb
+++ app/components/card_links/page_component.rb
@@ -1,13 +1,13 @@
 # typed: strict
 # frozen_string_literal: true
 
-module Cards
+module CardLinks
   class PageComponent < ApplicationComponent
-    sig { params(page: Page, show_topic_name: T::Boolean, class_name: String).void }
-    def initialize(page:, show_topic_name: true, class_name: "")
+    sig { params(page: Page, show_topic_name: T::Boolean, card_class: String).void }
+    def initialize(page:, show_topic_name: true, card_class: "")
       @page = page
       @show_topic_name = show_topic_name
-      @class_name = class_name
+      @card_class = card_class
     end
 
     sig { returns(Page) }
@@ -20,9 +20,19 @@ module Cards
     alias_method :show_topic_name?, :show_topic_name
 
     sig { returns(String) }
-    attr_reader :class_name
-    private :class_name
+    attr_reader :card_class
+    private :card_class
 
     delegate :space, :topic, to: :page
+
+    sig { returns(String) }
+    private def build_card_class
+      class_names(
+        card_class,
+        "bg-base-300 duration-200 ease-in-out grid min-h-[96px] transition px-3 py-2",
+        "hover:border hover:border-primary",
+        relative: page.pinned?
+      )
+    end
   end
 end
