4eed1aec3f672e9c8b1d5ad4cfeca113273e9970
Koji Shimba <me@shimba.co>
2025-07-13 17:09:13 +0900

    Fix

--- app/forms/pages/search_form.rb
+++ app/forms/pages/search_form.rb
@@ -3,18 +3,16 @@
 
 module Pages
   class SearchForm < ApplicationForm
-    extend T::Sig
-
     attribute :q, :string
 
-    validates :q, length: { 
-      minimum: 2, 
-      maximum: 100, 
+    validates :q, length: {
+      minimum: 2,
+      maximum: 100,
       allow_blank: true,
       too_short: "は%{count}文字以上で入力してください",
       too_long: "は%{count}文字以内で入力してください"
     }
-    validates :q, format: { with: /\A[^<>]*\z/, message: "不正な文字が含まれています" }
+    validates :q, format: {with: /\A[^<>]*\z/, message: "不正な文字が含まれています"}
 
     sig { returns(T::Boolean) }
     def q_present?
@@ -25,7 +23,7 @@ module Pages
     def searchable?
       valid_result = valid?
       return false unless valid_result == true
-      
+
       q_present?
     end
   end
