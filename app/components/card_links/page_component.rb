e289f92b414c9ef6e2f113deea5fb9759bfc3e68
Koji Shimba <me@shimba.co>
2025-07-17 01:34:11 +0900

    カードリンク内のスペース名やトピック名が長いときの見た目を調整

--- app/components/card_links/page_component.rb
+++ app/components/card_links/page_component.rb
@@ -8,10 +8,11 @@ module CardLinks
         page: Page,
         show_topic_name: T::Boolean,
         show_space_name: T::Boolean,
+        options: T::Hash[Symbol, String],
         card_class: String
       ).void
     end
-    def initialize(page:, show_topic_name: true, show_space_name: false, card_class: "")
+    def initialize(page:, show_topic_name: true, show_space_name: false, options: {}, card_class: "")
       # `show_space_name: true & show_topic_name: false` の組み合わせは想定していない
       if show_space_name && !show_topic_name
         raise ArgumentError, "show_space_name: true requires show_topic_name: true"
@@ -20,6 +21,7 @@ module CardLinks
       @page = page
       @show_topic_name = show_topic_name
       @show_space_name = show_space_name
+      @options = options
       @card_class = card_class
     end
 
@@ -37,11 +39,27 @@ module CardLinks
     private :show_space_name
     alias_method :show_space_name?, :show_space_name
 
+    sig { returns(T::Hash[Symbol, String]) }
+    attr_reader :options
+    private :options
+
     sig { returns(String) }
     attr_reader :card_class
     private :card_class
 
     delegate :space, :topic, to: :page
+
+    sig { returns(String) }
+    private def build_class_name
+      class_names(options[:class])
+    end
+
+    sig { returns(T::Hash[Symbol, String]) }
+    private def build_options
+      options.merge(
+        class: build_class_name
+      )
+    end
 
     sig { returns(String) }
     private def build_card_class
