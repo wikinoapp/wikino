6f08f631a42c5b48bd504b51ad06c60df9e0db00
Shimba, Koji <me@shimba.co>
2025-08-30 04:14:14 +0900

    権限定義の整理 (#879)

--- app/models/space_member_role.rb
+++ app/models/space_member_role.rb
@@ -2,34 +2,8 @@
 # frozen_string_literal: true
 
 class SpaceMemberRole < T::Enum
-  extend T::Sig
-
   enums do
     Owner = new("owner")
     Member = new("member")
-  end
-
-  sig { returns(T::Array[SpaceMemberPermission]) }
-  def permissions
-    case self
-    when Owner
-      [
-        SpaceMemberPermission::CreateTopic,
-        SpaceMemberPermission::CreatePage,
-        SpaceMemberPermission::CreateDraftPage,
-        SpaceMemberPermission::ExportSpace,
-        SpaceMemberPermission::UpdateSpace,
-        SpaceMemberPermission::UpdateTopic
-      ]
-    when Member
-      [
-        SpaceMemberPermission::CreateTopic,
-        SpaceMemberPermission::CreatePage,
-        SpaceMemberPermission::CreateDraftPage,
-        SpaceMemberPermission::UpdateTopic
-      ]
-    else
-      T.absurd(self)
-    end
   end
 end
