0a495858130991afb8b8321ad5afc9617c9a740a
Koji Shimba <me@shimba.co>
2025-08-29 19:13:42 +0900

    Fix

---
+++ app/policies/base_member_policy.rb
@@ -0,0 +1,84 @@
+# typed: strict
+# frozen_string_literal: true
+
+# 権限チェックの基底クラス
+# SpaceメンバーのPolicyクラスで共通して使用するロジックを提供
+class BaseMemberPolicy < ApplicationPolicy
+  sig do
+    params(
+      user_record: T.nilable(UserRecord),
+      space_member_record: T.nilable(SpaceMemberRecord)
+    ).void
+  end
+  def initialize(user_record:, space_member_record:)
+    @user_record = user_record
+    @space_member_record = space_member_record
+
+    # user_recordとspace_member_recordの関連性を検証
+    if mismatched_relations?
+      raise ArgumentError, [
+        "Mismatched relations.",
+        "user_record.id: #{user_record&.id.inspect}",
+        "space_member_record.user_id: #{space_member_record&.user_id.inspect}"
+      ].join(" ")
+    end
+  end
+
+  # スペースに参加しているかどうか
+  sig { returns(T::Boolean) }
+  def joined_space?
+    !space_member_record.nil?
+  end
+
+  # 指定されたスペースIDと同じスペースにいるかどうか
+  sig { params(space_id: String).returns(T::Boolean) }
+  def in_same_space?(space_id)
+    space_member_record&.space_id == space_id
+  end
+
+  # アクティブなメンバーかどうか
+  sig { returns(T::Boolean) }
+  def active?
+    space_member_record&.active? || false
+  end
+
+  # 参加しているトピックのレコードを取得
+  sig { returns(T.nilable(::ActiveRecord::Relation)) }
+  def joined_topic_records
+    return nil if space_member_record.nil?
+
+    space_member_record!.topic_records
+  end
+
+  # トピックに参加しているかどうか
+  sig { params(topic_id: String).returns(T::Boolean) }
+  def joined_topic?(topic_id)
+    return false if space_member_record.nil?
+
+    space_member_record!.topic_records.where(id: topic_id).exists?
+  end
+
+  protected
+
+  sig { returns(T.nilable(UserRecord)) }
+  attr_reader :user_record
+
+  sig { returns(T.nilable(SpaceMemberRecord)) }
+  attr_reader :space_member_record
+
+  # space_member_record の non-nil バージョン
+  sig { returns(SpaceMemberRecord) }
+  def space_member_record!
+    T.must(space_member_record)
+  end
+
+  private
+
+  # user_recordとspace_member_recordの関連性が不整合かどうか
+  sig { returns(T::Boolean) }
+  def mismatched_relations?
+    return false if user_record.nil? || space_member_record.nil?
+
+    T.must(user_record).id != T.must(space_member_record).user_id
+  end
+end
---
+++ spec/policies/base_member_policy_spec.rb
@@ -0,0 +1,181 @@
+# typed: false
+# frozen_string_literal: true
+
+require "rails_helper"
+
+# BaseMemberPolicyは抽象クラスなので、テスト用の具象クラスを定義
+class TestMemberPolicy < BaseMemberPolicy
+end
+
+RSpec.describe BaseMemberPolicy do
+
+  it "user_recordとspace_member_recordを受け取って初期化できること" do
+    user = FactoryBot.create(:user_record)
+    space = FactoryBot.create(:space_record)
+    space_member = FactoryBot.create(:space_member_record, user_record: user, space_record: space)
+
+    policy = TestMemberPolicy.new(user_record: user, space_member_record: space_member)
+
+    expect(policy).to be_instance_of(TestMemberPolicy)
+  end
+
+  it "user_recordとspace_member_recordの関連性が不一致の場合はエラーになること" do
+    user = FactoryBot.create(:user_record)
+    other_user = FactoryBot.create(:user_record)
+    space = FactoryBot.create(:space_record)
+    space_member = FactoryBot.create(:space_member_record, user_record: other_user, space_record: space)
+
+    expect do
+      TestMemberPolicy.new(user_record: user, space_member_record: space_member)
+    end.to raise_error(ArgumentError, /Mismatched relations/)
+  end
+
+  it "space_member_recordがnilの場合は初期化できること" do
+    user = FactoryBot.create(:user_record)
+
+    policy = TestMemberPolicy.new(user_record: user, space_member_record: nil)
+
+    expect(policy).to be_instance_of(TestMemberPolicy)
+  end
+
+  describe "#joined_space?" do
+    it "space_member_recordが存在する場合はtrueを返すこと" do
+      user = FactoryBot.create(:user_record)
+      space = FactoryBot.create(:space_record)
+      space_member = FactoryBot.create(:space_member_record, user_record: user, space_record: space)
+
+      policy = TestMemberPolicy.new(user_record: user, space_member_record: space_member)
+
+      expect(policy.joined_space?).to be(true)
+    end
+
+    it "space_member_recordがnilの場合はfalseを返すこと" do
+      user = FactoryBot.create(:user_record)
+
+      policy = TestMemberPolicy.new(user_record: user, space_member_record: nil)
+
+      expect(policy.joined_space?).to be(false)
+    end
+  end
+
+  describe "#in_same_space?" do
+    it "同じスペースに所属している場合はtrueを返すこと" do
+      user = FactoryBot.create(:user_record)
+      space = FactoryBot.create(:space_record)
+      space_member = FactoryBot.create(:space_member_record, user_record: user, space_record: space)
+
+      policy = TestMemberPolicy.new(user_record: user, space_member_record: space_member)
+
+      expect(policy.in_same_space?(space.id)).to be(true)
+    end
+
+    it "異なるスペースIDの場合はfalseを返すこと" do
+      user = FactoryBot.create(:user_record)
+      space = FactoryBot.create(:space_record)
+      other_space = FactoryBot.create(:space_record)
+      space_member = FactoryBot.create(:space_member_record, user_record: user, space_record: space)
+
+      policy = TestMemberPolicy.new(user_record: user, space_member_record: space_member)
+
+      expect(policy.in_same_space?(other_space.id)).to be(false)
+    end
+
+    it "space_member_recordがnilの場合はfalseを返すこと" do
+      user = FactoryBot.create(:user_record)
+
+      policy = TestMemberPolicy.new(user_record: user, space_member_record: nil)
+
+      expect(policy.in_same_space?("any_space_id")).to be(false)
+    end
+  end
+
+  describe "#active?" do
+    it "activeなメンバーの場合はtrueを返すこと" do
+      user = FactoryBot.create(:user_record)
+      space = FactoryBot.create(:space_record)
+      space_member = FactoryBot.create(:space_member_record, user_record: user, space_record: space, active: true)
+
+      policy = TestMemberPolicy.new(user_record: user, space_member_record: space_member)
+
+      expect(policy.active?).to be(true)
+    end
+
+    it "非activeなメンバーの場合はfalseを返すこと" do
+      user = FactoryBot.create(:user_record)
+      space = FactoryBot.create(:space_record)
+      space_member = FactoryBot.create(:space_member_record, user_record: user, space_record: space, active: false)
+
+      policy = TestMemberPolicy.new(user_record: user, space_member_record: space_member)
+
+      expect(policy.active?).to be(false)
+    end
+
+    it "space_member_recordがnilの場合はfalseを返すこと" do
+      user = FactoryBot.create(:user_record)
+
+      policy = TestMemberPolicy.new(user_record: user, space_member_record: nil)
+
+      expect(policy.active?).to be(false)
+    end
+  end
+
+  describe "#joined_topic?" do
+    it "トピックに参加している場合はtrueを返すこと" do
+      user = FactoryBot.create(:user_record)
+      space = FactoryBot.create(:space_record)
+      space_member = FactoryBot.create(:space_member_record, user_record: user, space_record: space)
+      topic = FactoryBot.create(:topic_record, space_record: space)
+      FactoryBot.create(:topic_member_record, space_member_record: space_member, topic_record: topic)
+
+      policy = TestMemberPolicy.new(user_record: user, space_member_record: space_member)
+
+      expect(policy.joined_topic?(topic.id)).to be(true)
+    end
+
+    it "トピックに参加していない場合はfalseを返すこと" do
+      user = FactoryBot.create(:user_record)
+      space = FactoryBot.create(:space_record)
+      space_member = FactoryBot.create(:space_member_record, user_record: user, space_record: space)
+      topic = FactoryBot.create(:topic_record, space_record: space)
+
+      policy = TestMemberPolicy.new(user_record: user, space_member_record: space_member)
+
+      expect(policy.joined_topic?(topic.id)).to be(false)
+    end
+
+    it "space_member_recordがnilの場合はfalseを返すこと" do
+      user = FactoryBot.create(:user_record)
+
+      policy = TestMemberPolicy.new(user_record: user, space_member_record: nil)
+
+      expect(policy.joined_topic?("any_topic_id")).to be(false)
+    end
+  end
+
+  describe "#joined_topic_records" do
+    it "参加しているトピックのレコードを返すこと" do
+      user = FactoryBot.create(:user_record)
+      space = FactoryBot.create(:space_record)
+      space_member = FactoryBot.create(:space_member_record, user_record: user, space_record: space)
+      topic1 = FactoryBot.create(:topic_record, space_record: space)
+      topic2 = FactoryBot.create(:topic_record, space_record: space)
+      FactoryBot.create(:topic_member_record, space_member_record: space_member, topic_record: topic1)
+      FactoryBot.create(:topic_member_record, space_member_record: space_member, topic_record: topic2)
+
+      policy = TestMemberPolicy.new(user_record: user, space_member_record: space_member)
+
+      topic_records = policy.joined_topic_records
+      expect(topic_records).to include(topic1)
+      expect(topic_records).to include(topic2)
+      expect(topic_records.count).to eq(2)
+    end
+
+    it "space_member_recordがnilの場合はnilを返すこと" do
+      user = FactoryBot.create(:user_record)
+
+      policy = TestMemberPolicy.new(user_record: user, space_member_record: nil)
+
+      expect(policy.joined_topic_records).to be_nil
+    end
+  end
+end
