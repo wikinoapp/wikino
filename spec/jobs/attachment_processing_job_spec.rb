a9d38464e552c0482975813daee27f31dc0b1e86
Shimba, Koji <me@shimba.co>
2025-09-08 02:27:27 +0900

    Fix

--- spec/jobs/attachment_processing_job_spec.rb
+++ spec/jobs/attachment_processing_job_spec.rb
@@ -8,10 +8,12 @@ RSpec.describe AttachmentProcessingJob, type: :job do
     it "存在しないattachment_recordの場合は何もしないこと" do
       service = instance_double(Attachments::ProcessService)
       allow(Attachments::ProcessService).to receive(:new).and_return(service)
-      expect(service).not_to receive(:call)
+      allow(service).to receive(:call)
 
       job = AttachmentProcessingJob.new
       job.perform("non-existent-id")
+
+      expect(service).not_to have_received(:call)
     end
 
     it "処理が成功した場合はリトライしないこと" do
@@ -21,11 +23,12 @@ RSpec.describe AttachmentProcessingJob, type: :job do
       service = instance_double(Attachments::ProcessService)
       allow(Attachments::ProcessService).to receive(:new).and_return(service)
       allow(service).to receive(:call).with(attachment_record:).and_return(result)
-
-      expect(AttachmentProcessingJob).not_to receive(:set)
+      allow(AttachmentProcessingJob).to receive(:set)
 
       job = AttachmentProcessingJob.new
       job.perform(attachment_record.id)
+
+      expect(AttachmentProcessingJob).not_to have_received(:set)
     end
 
     it "処理が失敗しpendingステータスの場合はリトライすること" do
@@ -39,10 +42,12 @@ RSpec.describe AttachmentProcessingJob, type: :job do
       # リトライジョブがスケジュールされることを確認
       job_wrapper = instance_double(ActiveJob::ConfiguredJob)
       allow(AttachmentProcessingJob).to receive(:set).with(wait: 5.seconds).and_return(job_wrapper)
-      expect(job_wrapper).to receive(:perform_later).with(attachment_record.id)
+      allow(job_wrapper).to receive(:perform_later)
 
       job = AttachmentProcessingJob.new
       job.perform(attachment_record.id)
+
+      expect(job_wrapper).to have_received(:perform_later).with(attachment_record.id)
     end
 
     it "処理が失敗しfailedステータスの場合はリトライしないこと" do
@@ -52,11 +57,12 @@ RSpec.describe AttachmentProcessingJob, type: :job do
       service = instance_double(Attachments::ProcessService)
       allow(Attachments::ProcessService).to receive(:new).and_return(service)
       allow(service).to receive(:call).with(attachment_record:).and_return(result)
-
-      expect(AttachmentProcessingJob).not_to receive(:set)
+      allow(AttachmentProcessingJob).to receive(:set)
 
       job = AttachmentProcessingJob.new
       job.perform(attachment_record.id)
+
+      expect(AttachmentProcessingJob).not_to have_received(:set)
     end
   end
 end
