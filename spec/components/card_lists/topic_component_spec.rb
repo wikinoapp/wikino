cb37380be7ef4d8551863f75044b8e7a9c0af605
Shimba, Koji <me@shimba.co>
2025-09-07 13:28:32 +0900

    Fix

--- spec/components/card_lists/topic_component_spec.rb
+++ spec/components/card_lists/topic_component_spec.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 RSpec.describe CardLists::TopicComponent, type: :view do
-  it "トピックカードのリストが表示されること" do
+  it "メンバー向けにトピックカードのリストが表示されること" do
     space_record = FactoryBot.create(:space_record)
     topic_record1 = FactoryBot.create(:topic_record, space_record:, name: "Topic 1")
     topic_record2 = FactoryBot.create(:topic_record, space_record:, name: "Topic 2")
@@ -16,7 +16,7 @@ RSpec.describe CardLists::TopicComponent, type: :view do
       )
     end
 
-    render_inline(CardLists::TopicComponent.new(topics:))
+    render_inline(CardLists::TopicComponent.new(topics:, is_guest: false))
 
     expect(page).to have_text("Topic 1")
     expect(page).to have_text("Topic 2")
@@ -24,8 +24,30 @@ RSpec.describe CardLists::TopicComponent, type: :view do
     expect(page).to have_css(".grid")
   end
 
+  it "ゲスト向けに公開トピックカードのリストが表示されること" do
+    space_record = FactoryBot.create(:space_record)
+    topic_record1 = FactoryBot.create(:topic_record, space_record:, name: "Public Topic 1", description: "Description 1")
+    topic_record2 = FactoryBot.create(:topic_record, space_record:, name: "Public Topic 2", description: "Description 2")
+
+    topics = [topic_record1, topic_record2].map do |topic_record|
+      TopicRepository.new.to_model(
+        topic_record:,
+        can_update: false,
+        can_create_page: false
+      )
+    end
+
+    render_inline(CardLists::TopicComponent.new(topics:, is_guest: true))
+
+    expect(page).to have_text("Public Topic 1")
+    expect(page).to have_text("Description 1")
+    expect(page).to have_text("Public Topic 2")
+    expect(page).to have_text("Description 2")
+    expect(page).to have_css(".grid")
+  end
+
   it "トピックがない場合に空のリストが表示されること" do
-    render_inline(CardLists::TopicComponent.new(topics: []))
+    render_inline(CardLists::TopicComponent.new(topics: [], is_guest: false))
 
     expect(page).to have_css(".grid")
     expect(page).not_to have_css("a")
