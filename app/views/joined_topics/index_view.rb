cd43df3babedd3c01085b14243fa39370db9131d
Shimba, Koji <me@shimba.co>
2025-09-13 19:43:03 +0900

    Fix

--- app/views/joined_topics/index_view.rb
+++ app/views/joined_topics/index_view.rb
@@ -3,13 +3,23 @@
 
 module JoinedTopics
   class IndexView < ApplicationView
-    sig { params(topics: T::Array[Topic]).void }
-    def initialize(topics:)
+    sig { params(topics: T::Array[Topic], variant: Symbol).void }
+    def initialize(topics:, variant:)
       @topics = topics
+      @variant = variant
     end
 
     sig { returns(T::Array[Topic]) }
     attr_reader :topics
     private :topics
+
+    sig { returns(Symbol) }
+    attr_reader :variant
+    private :variant
+
+    sig { returns(String) }
+    private def turbo_frame_id
+      "joined-topics-#{variant}"
+    end
   end
 end
