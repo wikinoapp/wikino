8696bd00c7fc40f18a851510bfd8cade726f03e3
Shimba, Koji <me@shimba.co>
2025-09-16 02:01:21 +0900

    Fix

--- app/javascript/controllers/stuck-controller.ts
+++ app/javascript/controllers/stuck-controller.ts
@@ -1,30 +1,37 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller<HTMLElement> {
-  declare ticking: boolean;
+  declare observer: IntersectionObserver;
 
   connect() {
-    this.ticking = false;
+    // IntersectionObserverを使用してスティッキー状態を検出
+    // rootMargin: "-1px 0px 0px 0px"により、要素が画面上端を1px超えたときに検出
+    this.observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          // intersectionRatio < 1 は要素が部分的に隠れている（スティッキー状態）
+          // boundingClientRect.top <= 0 は要素が画面上端以上にある
+          if (!entry.isIntersecting || (entry.intersectionRatio < 1 && entry.boundingClientRect.top <= 0)) {
+            this.element.dataset.stuck = "";
+          } else {
+            delete this.element.dataset.stuck;
+          }
+        });
+      },
+      {
+        // 要素が画面上端に到達したときを正確に検出
+        rootMargin: "-1px 0px 0px 0px",
+        threshold: [0, 1],
+      }
+    );
 
-    window.addEventListener("scroll", this.handleScroll.bind(this), { passive: true });
+    this.observer.observe(this.element);
   }
 
-  handleScroll() {
-    if (!this.ticking) {
-      window.requestAnimationFrame(() => {
-        const elementTop = this.element.getBoundingClientRect().top;
-        const currentScrollPos = window.scrollY;
-
-        if (elementTop <= 0 && currentScrollPos > 0) {
-          this.element.dataset.stuck = "";
-        } else {
-          delete this.element.dataset.stuck;
-        }
-
-        this.ticking = false;
-      });
-
-      this.ticking = true;
+  disconnect() {
+    // クリーンアップ：要素が削除されるときにobserverを停止
+    if (this.observer) {
+      this.observer.disconnect();
     }
   }
 }
