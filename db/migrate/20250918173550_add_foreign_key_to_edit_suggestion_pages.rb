0ad550f6a4af9f928c37eb93687b42e700e9e747
Shimba, Koji <me@shimba.co>
2025-09-21 18:05:48 +0900

    Fix

---
+++ db/migrate/20250918173550_add_foreign_key_to_edit_suggestion_pages.rb
@@ -0,0 +1,9 @@
+# typed: false
+# frozen_string_literal: true
+
+class AddForeignKeyToEditSuggestionPages < ActiveRecord::Migration[8.0]
+  def change
+    add_foreign_key :edit_suggestion_pages, :edit_suggestion_page_revisions,
+                    column: :latest_revision_id, validate: false
+  end
+end
